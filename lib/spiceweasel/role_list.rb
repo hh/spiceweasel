9a67db21abdca522c224ff96e356073fe2609201
Chris McClimans <chris@hippiehacker.org>
2011-12-14 11:56:40 +1300

    roles need to be loaded from roles/*

--- lib/spiceweasel/role_list.rb
+++ lib/spiceweasel/role_list.rb
@@ -6,7 +6,7 @@ class Spiceweasel::RoleList
       roles.each do |role|
         STDOUT.puts "DEBUG: role: #{role.keys[0]}" if DEBUG
         @delete += "knife role#{options['knife_options']} delete #{role.keys[0]} -y\n"
-        @create += "knife role#{options['knife_options']} from file #{role.keys[0]}.rb\n"
+        @create += "knife role#{options['knife_options']} from file roles/#{role.keys[0]}.rb\n"
         @roles << role.keys[0]
       end
     end
